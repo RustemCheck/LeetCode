07f2fc5c91702c8e9774bfff654d4f5ac4e7d37b
RustemCheck <rustemzinnatullin165@gmail.com>
2024-04-01 22:15:14 +0300

    longestCommonPrefix

---
+++ TaskLongestCommonPrefix/src/TaskLongestCommonPrefix.java
@@ -0,0 +1,8 @@
+public class TaskLongestCommonPrefix {
+    public static void main(String[] args) {
+
+    }
+    public static String longestCommonPrefix(String[] strs) {
+        return "";
+    }
+}
